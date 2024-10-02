dc088c83f1e50a271d05ad26b3ed88d120076f37
yoyo <yo011shh@naver.com>
2024-10-02 17:56:30 +0900

    4주차 작성완료

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,7 +1,11 @@
-package com.example.demo;
+package com.example.demo.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.model.domain.TestDB;
+import com.example.demo.model.service.TestService;
 @Controller // 컨트롤러 어노테이션 명시
 public class DemoController
 {
@@ -29,5 +33,23 @@ return "hello"; // hello.html 연결
 public String about() {
 return "about_detailed";
 }
-
+@GetMapping("/test1")
+public String thymeleaf_test1(Model model) {
+model.addAttribute("data1", "<h2> 방갑습니다 </h2>");
+model.addAttribute("data2", "태그의 속성 값");
+model.addAttribute("link", 01);
+model.addAttribute("name", "홍길동");
+model.addAttribute("para1", "001");
+model.addAttribute("para2", 002);
+return "thymeleaf_test1";
+}
+@Autowired
+    private TestService testService;
+@GetMapping("/testdb")
+public String getAllTestDBs(Model model) {
+TestDB test = testService.findByName("홍길동");
+model.addAttribute("data4", test);
+System.out.println("데이터 출력 디버그 : " + test);
+return "testdb";
+}
 } 
---
+++ demo/src/main/java/com/example/demo/model/domain/TestDB.java
@@ -0,0 +1,13 @@
+package com.example.demo.model.domain;
+import jakarta.persistence.*;
+import lombok.Data;
+@Entity // TestDB라는 객체와 DB 테이블을 매핑. JPA가 관리
+@Table(name = "testdb") // 테이블 이름은 testdb
+@Data // set/get/tostring 등 필수 어노테이션 자동 생성
+public class TestDB {
+    @Id // 해당 변수가 PK
+@GeneratedValue(strategy = GenerationType.IDENTITY) // 값이 없어도 자동으로 할당
+private Long id;
+@Column(nullable = true) // 테이블의 컬럼 설정 값을 명시
+private String name;
+}
