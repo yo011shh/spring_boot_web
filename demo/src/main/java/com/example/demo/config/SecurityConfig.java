ab1e340aa265c1b9a7de53d491374416180f47be
yoyo <yo011shh@naver.com>
2024-12-05 19:08:45 +0900

    11주차 완료

--- demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -19,7 +19,8 @@ public class SecurityConfig { // 스프링에서 보안 관리 클래스
         response.setHeader("X-XSS-Protection", "1; mode=block"); // XSS-Protection 헤더 설정
         })
         )
-        .csrf(withDefaults())
+        //.csrf(withDefaults())
+        .csrf(csrf -> csrf.disable())
         .sessionManagement(session -> session
         .invalidSessionUrl("/session-expired") // 세션 만료시 이동 페이지
         .maximumSessions(1) // 사용자 별 세션 최대 수
